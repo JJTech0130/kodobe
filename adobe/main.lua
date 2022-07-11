0e5ba2b75bb3b9df0c6b037c2395ec0b6ff0b9ce
JJTech <jjtech@jjtech.dev>
2022-07-10 21:06:51 -0400

    add test sign in request construction to main

--- adobe/main.lua
+++ adobe/main.lua
@@ -3,12 +3,24 @@ package.path = ";./dependencies/?.lua;./dependencies/?/init.lua" .. package.path
 local adobe = require("adobe.adobe")
 local crypto = require("adobe.util.crypto")
 local templates = require("adobe.templates")
+local base64 = require("adobe.util.util").base64
 
 -- main
 --local authServiceInfo = adobe.getAuthenticationServiceInfo()
 --local activationCert = adobe.getActivationServiceCertificate()
 local deviceKey = crypto.generateDeviceKey()
--- encrypto anon credentials (no use an password)
---local login = crypto.encryptLogin("", "", deviceKey, authServiceInfo.certificate)
+-- encrypt anonymous credentials (no username or password)
+local login = crypto.encryptLogin("", "", deviceKey, authServiceInfo.certificate)
+--local login = "TEST"
 --print(templates.signInRequest("anonymous", login))
-crypto.generateKey(deviceKey)
+local authKey = crypto.generateKey(deviceKey)
+local licenseKey = crypto.generateKey(deviceKey)
+
+local signInRequest = templates.signInRequest(
+    "anonymous", login, 
+    base64.encode(authKey.public), 
+    base64.encode(authKey.encrypted), 
+    base64.encode(licenseKey.public), 
+    base64.encode(licenseKey.encrypted)
+)
+print(signInRequest)
