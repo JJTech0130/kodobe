4ec670512c224a801dc73bd4f760e6e941e13701
JJTech <jjtech@jjtech.dev>
2022-07-11 08:19:13 -0400

    cleanup, need table.concat

--- adobe/adobe.lua
+++ adobe/adobe.lua
@@ -45,8 +45,8 @@ function adobe.signIn(method, username, password)
     local authKey = crypto.generateKey(deviceKey)
     local licenseKey = crypto.generateKey(deviceKey)
 
-    print(base64.encode(authKey.private))
     local login = crypto.encryptLogin(username, password, deviceKey, authCert)
+
     local signInRequest = xml.adobe({
         _attr = { method = method},
         signInData = login,
@@ -55,9 +55,7 @@ function adobe.signIn(method, username, password)
         publicLicenseKey = base64.encode(licenseKey.public),
         encryptedPrivateLicenseKey = base64.encode(licenseKey.encrypted)
     }, "signIn")
-    print(signInRequest)
 
-    -- send POST with type application/vnd.adobe.adept+xml
     headers = {}
     headers["Content-Type"] = "application/vnd.adobe.adept+xml"
     headers["Accept"] = "*/*"
@@ -71,10 +69,9 @@ function adobe.signIn(method, username, password)
         headers = headers,
         source = ltn12.source.string(signInRequest)
     }
-    
-    --local response = http.request(url.build(util.endpoint(adobe.EDEN_URL, "SignInDirect")), "POST", signInRequest, headers)
-    print(resp[1])
-    --print(signInRequest)
+    resp = table.concat(resp)
+
+    print(resp)
 end
 
 return adobe
