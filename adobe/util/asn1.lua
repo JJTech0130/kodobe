fadd5e9f9531fe47d5e24d342cfb83e243f1897b
JJTech <jjtech@jjtech.dev>
2022-07-12 09:34:12 -0400

    asn1: add sorting

--- adobe/util/asn1.lua
+++ adobe/util/asn1.lua
@@ -1,5 +1,50 @@
 --- @module encodes table as ASN.1 DER
 
+-- sorting stuff
+local function __genOrderedIndex( t )
+    local orderedIndex = {}
+    for key in pairs(t) do
+        table.insert( orderedIndex, key )
+    end
+    table.sort( orderedIndex )
+    return orderedIndex
+end
+
+local function orderedNext(t, state)
+    -- Equivalent of the next function, but returns the keys in the alphabetic
+    -- order. We use a temporary ordered key table that is stored in the
+    -- table being iterated.
+
+    local key = nil
+    --print("orderedNext: state = "..tostring(state) )
+    if state == nil then
+        -- the first time, generate the index
+        t.__orderedIndex = __genOrderedIndex( t )
+        key = t.__orderedIndex[1]
+    else
+        -- fetch the next value
+        for i = 1,table.getn(t.__orderedIndex) do
+            if t.__orderedIndex[i] == state then
+                key = t.__orderedIndex[i+1]
+            end
+        end
+    end
+
+    if key then
+        return key, t[key]
+    end
+
+    -- no more value to return, cleanup
+    t.__orderedIndex = nil
+    return
+end
+
+local function orderedPairs(t)
+    -- Equivalent of the pairs() function on tables. Allows to iterate
+    -- in order
+    return orderedNext, t, nil
+end
+
 local bit = require("bit")
 
 local ASN = {
@@ -49,7 +94,7 @@ function ASN.tag(name)
 end
 
 function ASN.attribute(name, value)
-    -- don't add xmlns attributes, as namespaces are fully qualifieds
+    -- don't add xmlns attributes, as namespaces are fully qualified
     if string.find(name, "xmlns:") then
         return ""
     end
@@ -62,7 +107,7 @@ function ASN.element(name, content)
     out = out .. ASN.tag(name)
     if content._attr ~= nil then
         -- FIXME: sort attributes
-        for k, v in pairs(content._attr) do
+        for k, v in orderedPairs(content._attr) do
             out = out .. ASN.attribute(k, v)
         end
         content._attr = nil
@@ -76,7 +121,7 @@ function ASN.element(name, content)
         out = out .. ASN.string(content)
     elseif type(content) == 'table' then
         -- FIXME: sort elements
-        for k, v in pairs(content) do
+        for k, v in orderedPairs(content) do
             out = out .. ASN.element(k, v)
         end
     end
